134960ea326ef072c63edf2fcfb91947a006dd9f
Javier Diaz Urbano <me@xavidiaz.com>
2026-06-05 09:32:33 +0200

    feat(del3): add Appliance base class with virtual methods

--- Program.cs
+++ Program.cs
@@ -1,6 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 
+public class Appliance
+{
+    public string Brand { get; }
+    public string Room { get; }
+    public bool IsOn { get; protected set; }
+
+    public Appliance(string brand, string room)
+    {
+        Brand = brand;
+        Room = room;
+    }
+
+    public virtual string GetInfo()
+    {
+        return $"{Brand} in {Room} room";
+    }
+
+    public virtual void TurnOn()
+    {
+        IsOn = true;
+    }
+
+    public virtual void TurnOff()
+    {
+        IsOn = false;
+        Console.WriteLine($"{Brand} has stopped.");
+    }
+
+    public virtual double GetDailyEnergyUsage()
+    {
+        // Returnera 0 som standardvärde.
+        return 0;
+    }
+}
+
 public class Washer
 {
     public string Brand { get; }
